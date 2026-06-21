98a81f9e90362617209df4f48babb3c5c58c0872
Mr.X <111974179+Weirdo-1224@users.noreply.github.com>
2026-06-21 15:57:24 +0800

    chore: initialize forum repository

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Forum Backend
+
+使用 OpenSpec-cn 和 Codex 开发的 Spring Boot 博客论坛后端。
---
+++ src/test/java/org/example/forum_system/ForumSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.forum_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
